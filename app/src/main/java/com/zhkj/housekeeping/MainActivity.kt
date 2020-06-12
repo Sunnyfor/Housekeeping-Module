3848abe89b8898f444dce4d688ff94a6eb0d9f24
ZhangYe <zhangye98@foxmail.com>
2020-06-12 19:22:38 +0800

    修改首页容器的继承、删除无用代码

--- app/src/main/java/com/zhkj/housekeeping/MainActivity.kt
+++ app/src/main/java/com/zhkj/housekeeping/MainActivity.kt
@@ -1,53 +1,29 @@
 package com.zhkj.housekeeping
 
-import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+import android.view.View
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.alibaba.android.arouter.facade.annotation.Route
+import com.sunny.zy.base.BaseActivity
 import com.sunny.zy.utils.RouterPath
 import kotlinx.android.synthetic.main.activity_main.*
 
 @Route(path = RouterPath.APP_MAIN_ACTIVITY)
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        bottom_navigation.setupWithNavController(nav_host_fragment.findNavController())
-//
+    override fun setLayout(): Int = R.layout.activity_main
+
+    override fun initView() {
+
     }
 
+    override fun loadData() {
+        bottom_navigation.setupWithNavController(nav_host_fragment.findNavController())
+    }
 
-//
-//    override fun initView() {
-//
-//        //导航切换
-//        bottom_navigation.setOnNavigationItemSelectedListener {
-//
-//            when(it.title){
-//                getString(R.string.workbench) ->{
-//                    ToastUtil.show("条件成立1")
-//                }
-//                getString(R.string.message) -> {
-//                    ToastUtil.show("条件成立2")
-//                }
-//
-//                getString(R.string.mine) -> {
-//                    ToastUtil.show("条件成立3")
-//                }
-//            }
-//            return@setOnNavigationItemSelectedListener true
-//        }
-//    }
-//
-//    override fun loadData() {
-//
-//    }
-//
-//    override fun onClickEvent(view: View) {
-//
-//    }
+    override fun onClickEvent(view: View) {
+
+    }
 
 }
